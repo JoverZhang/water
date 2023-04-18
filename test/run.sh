4d586ad37569b3e30f825c0b98ece42ed14bb5b5
JoverZhang <joverzh@gmail.com>
2023-04-18 14:09:24 +0800

    test: refactor run.sh

--- test/run.sh
+++ test/run.sh
@@ -6,8 +6,19 @@ export DKM_REQUIRE_SEARCH_PATH="$SEARCH_PATH"
 
 WATER="cmake-*/water"
 
-# std
-$WATER require/water/collections/map.water
+tests=(
+  # std
+  require/water/collections/map.water
 
-# tests
-$WATER test/case.water
+  # tests
+  test/case.water
+)
+
+for t in "${tests[@]}" ; do
+    echo -e "\e[34mRUN TEST: $t\e[0m"
+    $WATER "$t"
+    ret="$?"
+    if [[ "$ret" -ne 0 ]]; then
+      exit "$ret"
+    fi
+done
