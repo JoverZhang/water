dff1077b033d23ca0bb23a55bbecbbad1f2e27f0
JoverZhang <joverzh@gmail.com>
2023-04-18 23:55:32 +0800

    chore: split CMake test cases

--- test/run.sh
+++ test/run.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-SEARCH_PATH="$PWD/require:$PWD/test"
+export DKM_REQUIRE_SEARCH_PATH="$PWD/require:$PWD/test"
 
-export DKM_REQUIRE_SEARCH_PATH="$SEARCH_PATH"
+WATER="$1"
+FILE="$2"
 
-WATER="cmake-*/water"
-
-tests=(
-  # std
-  require/water/collections/map.water
-  require/water/fs.water
-
-  # tests
-  test/case.water
-)
-
-for t in "${tests[@]}" ; do
-    echo -e "\e[34mRUN TEST: $t\e[0m"
-    $WATER "$t"
-    ret="$?"
-    if [[ "$ret" -ne 0 ]]; then
-      exit "$ret"
-    fi
-done
+echo -e "\e[34mRUN TEST: $FILE\e[0m"
+$WATER "$FILE"
+exit "$?"
