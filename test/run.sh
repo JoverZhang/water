5c99e2d48866cad9ef0c14ecb6b96f869053423d
JoverZhang <joverzh@gmail.com>
2023-04-30 03:15:33 +0800

    test: improve run.sh for supports test main()

--- test/run.sh
+++ test/run.sh
@@ -3,8 +3,17 @@
 export WATER_HOME="$PWD/require:$PWD/test"
 
 WATER="$1"
-FILE="$2"
+CMD="$2"
+FILE="$3"
 
 echo -e "\e[34mRUN TEST: $FILE\e[0m"
-$WATER test "$FILE"
+if [ "$CMD" = 'TEST' ]; then
+  $WATER test "$FILE"
+elif [ "$CMD" = 'RUN' ]; then
+  $WATER "$FILE"
+else
+  echo -e "\e[31mtest/run.sh: unknown command '$CMD\e[0m'"
+  exit 1
+fi
+
 exit "$?"
